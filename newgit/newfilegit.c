b75a782bf7c9aefddbd3706638ff2b3e0a93cfc9
nishakalakoti <kalakotijiinishagmail.com>
2024-06-11 19:00:04 +0530

    initialcommit

---
+++ newgit/newfilegit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ char array[1][10];
+ printf("enter name\n");
+ for ( int i = 0; i < 1; i++)
+ {
+    scanf("\n%s",&array[i]);
+ }
+ for (int i = 0; i <1; i++)
+ {
+    printf("\n%s",array[i]);
+ }
+
+ return 0;
+}
