ad81ef33e74948f482c49ed92eb8a29d6e51e010
nishakalakoti <kalakotijiinishagmail.com>
2024-06-12 08:15:50 +0530

    initialcommit

---
+++ namepeint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char name [1][20];
+    printf("enter any name");
+    for (int i = 0; i < 1; i++)
+    {
+        scanf("\n%s",&name[i]);
+    }
+    for (int i = 0; i < 1; i++)
+    {
+        printf("%s",name[i]);
+    }
+    return 0;
+    
+}
