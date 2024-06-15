8b84df18d82cfa42150f0d32b345e1aee7a12fa8
nishakalakoti <kalakotijiinisha@gmail.com>
2024-06-15 10:14:42 +0530

    update table

---
+++ table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int l;
+    printf("enter any number");
+    scanf("%d",&l);
+    for (int i = 1; i<=10; i++)
+    {
+        int g=l*i;
+        printf("%d*%d=%d",l,i,g);
+        printf("\n");
+    }
+    return 0;
+    
+}
