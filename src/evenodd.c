01b3ba3c093a2b673ad9e6dd7b65a5c500ba4fca
nishakalakoti <kalakotijiinisha@gmail.com>
2024-06-15 16:10:34 +0530

    find even

---
+++ src/evenodd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("please enter the number");
+    scanf("%d",&number);
+
+    if(number%2==0)
+    {
+        printf("this is even number%d",number);
+    }
+    else
+    {
+       printf("this is odd number%d",number);
+    }
+    return 0;
+
+}
