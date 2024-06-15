a20e49e87400689ab5901fb1cf232073d73edc02
nishakalakoti <kalakotijiinisha@gmail.com>
2024-06-15 09:59:56 +0530

    update table

--- findtable.c
+++ findtable.c
@@ -7,7 +7,7 @@ int main()
     for (int i = 1; i <=10; i++)
     {
         int y=s*i;
-        printf("\n%d*%d=%d",s,i,y);
+        printf("%d*%d=%d",s,i,y);
         printf("\n");
         
     }
